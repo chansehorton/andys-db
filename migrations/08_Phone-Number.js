ae5a40b78569f2d7513f400dd324d9da991c9daf
CHANSE HORTON <chansehorton@yahoo.com>
2017-08-20 12:14:06 -0600

    add phone-numbers table

--- migrations/08_Phone-Number.js
+++ migrations/08_Phone-Number.js
@@ -1,8 +1,14 @@
-
+'use strict'
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('Phone-Number', (table) => {
+    table.increments();
+    table.string('number').notNullable();
+    table.string('type');
+    table.integer('person-id').references('id').inTable('Person');
+    table.integer('company-id').references('id').inTable('Company');
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('Phone-Number');
 };
